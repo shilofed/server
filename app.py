a0ea298a15f924100ee8f47a6dcb37753d28f008
shilofed <77000772+shilofed@users.noreply.github.com>
2022-04-27 02:28:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,20 @@ import games_stats_factory
 import json
 import game_stats
 import ast
-from refresh_data import Game
 
 app = Flask(__name__)
 
 
 @app.route("/<arg>")
 def get_best_game(arg):
-    # print(arg)
-    # with open("games.gms", "rb") as f:
-    #     games = pickle.load(f)
+    class Game:
+        def __init__(self, game_stats):
+            self.game = game_stats
+            self.comeback = game_stats.get_score_greatest_comeback()
+            self.close_game = game_stats.get_score_close_game()
+            self.best_teams = game_stats.get_score_best_teams()
+            self.personal_performance = game_stats.get_score_personal_performance()
+            self.high_game_rate = game_stats.get_score_game_rate()
     biggest_score = -np.inf
     pref = arg.split(";")
     pref = list(filter(bool, pref))
@@ -30,17 +34,15 @@ def get_best_game(arg):
     for game_class in games:
         score = 0
         if 'Great_comeback' in pref_dict:
-            score += int(pref_dict['Great comeback']) * game_class.comeback
+            score += int(pref_dict['Great_comeback']) * game_class.comeback
         if 'Close_game' in pref_dict:
-            score += int(pref_dict['Close game']) * game_class.close_game
+            score += int(pref_dict['Close_game']) * game_class.close_game
         if 'Good_teams' in pref_dict:
-            score += int(pref_dict['Good teams']) * game_class.best_teams
+            score += int(pref_dict['Good_teams']) * game_class.best_teams
         if 'High_game_rate' in pref_dict:
-            score += int(pref_dict['High game rate']) * game_class.high_game_rate
+            score += int(pref_dict['High_game_rate']) * game_class.high_game_rate
         if 'personal_performance' in pref_dict:
-            score += int(pref_dict['personal performance']) * game_class.personal_performance
-
-        # print("score= ", score)
+            score += int(pref_dict['personal_performance']) * game_class.personal_performance
         if score > biggest_score:
             biggest_score = score
             best_score = game_class.game
@@ -51,13 +53,12 @@ def get_best_game(arg):
         json_response["response_tag"] = 1
         json_response["team_1_id"] = int(best_score.line_score.TEAM_ID[0])
         json_response["team_2_id"] = int(best_score.line_score.TEAM_ID[1])
-    # print("biggest score = ", biggest_score)
-    # print("return")
     return json.dumps(json_response)
+
 
 @app.route("/")
 def hello():
-    return 'hello world'
+    return 'hello worldfsfd'
 
 if __name__ == "__main__":
 
